b870baca29e2c5c1ae809a6575ab3b397182b046
festus <kiamfestoh@gmail.com>
2019-06-14 13:02:27 +0300

    create cats data source interface

--- app/src/main/java/com/example/kittytinder/data/source/CatsDataSource.kt
+++ app/src/main/java/com/example/kittytinder/data/source/CatsDataSource.kt
@@ -1,7 +1,14 @@
 package com.example.kittytinder.data.source
 
+import com.example.kittytinder.data.CatImage
+import com.example.kittytinder.data.Vote
+
 /**
  * Created by Festus Kiambi on 6/14/19.
  */
 interface CatsDataSource {
+
+    suspend fun getCatImages(): List<CatImage>?
+
+    suspend fun saveVote(vote : Vote)
 }
