bfe88048ef55551e93159e9667a8123715788391
festus <kiamfestoh@gmail.com>
2019-06-17 03:44:20 +0300

    add vote entity

--- app/src/main/java/com/example/kittytinder/data/Vote.kt
+++ app/src/main/java/com/example/kittytinder/data/Vote.kt
@@ -1,24 +1,34 @@
 package com.example.kittytinder.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.squareup.moshi.Json
 
+@Entity(tableName = "votes")
 data class Vote(
 
+	@Json(name="id")
+	@PrimaryKey
+	val id: Int? = null,
+
 	@Json(name="country_code")
+	@ColumnInfo(name = "country_code")
 	val countryCode: Any? = null,
 
 	@Json(name="sub_id")
+	@ColumnInfo(name = "sub_id")
 	val subId: Any? = null,
 
 	@Json(name="created_at")
+	@ColumnInfo(name = "created_at")
 	val createdAt: String? = null,
 
-	@Json(name="id")
-	val id: Int? = null,
-
 	@Json(name="image_id")
+	@ColumnInfo(name = "image_id")
 	val imageId: String? = null,
 
 	@Json(name="value")
+	@ColumnInfo(name = "value")
 	val value: Int? = null
 )
