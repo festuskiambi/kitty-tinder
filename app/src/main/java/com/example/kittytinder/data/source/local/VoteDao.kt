bf02c654f6a72b708fb1206a656f65070b1251b5
festus <kiamfestoh@gmail.com>
2019-06-17 03:47:26 +0300

    add Dao

---
+++ app/src/main/java/com/example/kittytinder/data/source/local/VoteDao.kt
@@ -0,0 +1,15 @@
+package com.example.kittytinder.data.source.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.example.kittytinder.data.Vote
+
+/**
+ * Created by Festus Kiambi on 6/17/19.
+ */
+@Dao
+ abstract class VoteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertPost(vote :Vote)
+}
